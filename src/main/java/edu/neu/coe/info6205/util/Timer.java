2a51c4867f734aff84b78c60c467d22a2cdd928c
Sai Saketh k <90648349+NuSaketh@users.noreply.github.com>
2021-09-26 23:23:50 -0400

    Update Timer.java

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -55,7 +55,25 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // TO BE IMPLEMENTED: note that the timer is running when this method is called and should still be running when it returns.
-        return 0;
+        
+        pause();
+        for(int i =0; i<n; i++)
+        {
+        	if(preFunction != null) {
+        		preFunction.apply(supplier.get());        	}
+        
+        resume();
+        U r = function.apply(supplier.get());
+        pauseAndLap();
+        if(postFunction != null) {
+        	postFunction.accept(r);
+        }
+        
+    }
+       
+        
+        return meanLapTime();
+    
     }
 
     /**
@@ -174,7 +192,9 @@ public class Timer {
      */
     private static long getClock() {
         // TO BE IMPLEMENTED
-        return 0;
+    	
+    	
+        return System.nanoTime();
     }
 
     /**
@@ -186,7 +206,9 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // TO BE IMPLEMENTED
-        return 0;
+    	 
+    	 
+        return ticks/1000000;
     }
 
     final static LazyLogger logger = new LazyLogger(Timer.class);
